94ce08da22593fe7d3648ae471c2caa2daa3ba8b
diogosalesdev <diogosalesdev@gmail.com>
2020-06-07 13:02:45 -0300

    Using HTTP Methods and params with insomnia and installing nodemon

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,33 @@ const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(express.json());
+
+/*
+Métodos HTTP:
+
+GET: Buscar uma informação do Back-end
+POST: Criar uma informação no Back-end
+PUT: Alterar uma informação no Back-end
+DELETE: Deletar uma informação no Back-end
+
+Tipos de parâmetros
+
+Query Params: Parâmetros nomeados enviados na rota após "?" (filtros, paginação)
+Route Params: Parâmetros utilizados para identificar recursos
+Request Body: Corpo da requisição utilizado para criar ou alterar recursos
+
+*/
+app.post('/', (req, res) => {
+  const body = req.body;
+
+  console.log(body)
+
   return res.json({
     evento: 'Semana Omnistack 11.0',
-    aluno: 'Diogo Sales'
+    aluno: 'Diego Fernandes'
   });
 });
+
 
 app.listen(3333);
