a615c9ffbe5d7e4319bb7d8cb636fdbc90b260ec
diogosalesdev <diogosalesdev@gmail.com>
2020-06-08 19:02:56 -0300

    All routes finished

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -18,5 +18,22 @@ module.exports = {
       ong_id,
     });
     return res.json({ id });
+  },
+
+  async delete(req, res) {
+    const { id } = req.params;
+    const ong_id = req.headers.authorization;
+
+    const incident = await connection('incidents')
+      .where('id', id)
+      .select('ong_id')
+      .first()
+    if (incident.ong_id != ong_id) {
+      return res.status(401).json({ error: 'Operation not permitted!' });
+    } else {
+      await connection('incidents').where('id', id).delete();
+      return res.status(204).send();
+    }
+   
   }
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,15 +2,22 @@ const express = require('express');
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
+routes.post('/session', SessionController.create);
+
 //Lista as ongs cadastradas
-routes.get('/ongs', OngController.index)
+routes.get('/ongs', OngController.index);
 //Cadastra ongs
 routes.post('/ongs', OngController.create);
 
-routes.get('/incidents', IncidentController.index)
+routes.get('/profiles', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
