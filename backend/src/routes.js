7b4fa567c1c0be1bca17fc3f3409da53aaebc3df
diogosalesdev <diogosalesdev@gmail.com>
2020-06-08 13:38:37 -0300

    Create and List routes finished

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
 
 const routes = express.Router();
 
@@ -8,5 +9,8 @@ const routes = express.Router();
 routes.get('/ongs', OngController.index)
 //Cadastra ongs
 routes.post('/ongs', OngController.create);
+
+routes.get('/incidents', IncidentController.index)
+routes.post('/incidents', IncidentController.create);
 
 module.exports = routes;
