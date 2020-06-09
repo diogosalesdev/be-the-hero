141c102d90652d1a8ef098568abe3673f17c8a77
diogosalesdev <diogosalesdev@gmail.com>
2020-06-08 22:13:23 -0300

    React concepts learned

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 
 //JSX (Java script e XML)
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter(counter+1);
+    
+  }
   return (
-    <Header>
-      Semana Omnistack  
-    </Header>
+    <div>
+      <Header>
+        Contador: {counter}  
+      </Header>
+      <button onClick={increment}>Incrementar</button>
+    </div>
   );
 }
 
